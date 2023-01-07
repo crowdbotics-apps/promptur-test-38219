09176f4b7ba0fe724c315d9dba50265917b749f6
Crowdbotics <team@crowdbotics.com>
2023-01-07 15:33:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "promptur-test-38219",
+  url: "https://promptur-test-38219.botics.co",
+  api: "https://promptur-test-38219.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
